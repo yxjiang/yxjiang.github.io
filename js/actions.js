491d10be7c7506c5f1591515ac3918c13a9a9c9e
yexi <yexijiang@gmail.com>
2014-03-05 08:54:09 -0500

    update

--- js/actions.js
+++ js/actions.js
@@ -82,9 +82,9 @@ $(function(){
     var services = data["services"]
     var reviewer = services["reviewer"]
     $("#services").append("<div id='reviewer'><ul id='review-list'></ul></div>");
-    $.each(reviewer, function(reviewer_index, reviewer_entry) {
+    //$.each(reviewer, function(reviewer_index, reviewer_entry) {
       //$("#review-list").append("<li>" + reviewer_entry + "</li>");
-    }
+    //}
 
     //	initialize code page
     var codes = data["codes"];
