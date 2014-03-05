5fc059c9d13f949e5d650ae586fab496536aa315
yexi <yexijiang@gmail.com>
2014-03-05 09:03:22 -0500

    update

--- js/actions.js
+++ js/actions.js
@@ -80,11 +80,11 @@ $(function(){
     });
 
     var services = data["services"]
-    var reviewer = services["reviewer"]
-    $("#services").append("<div id='reviewer'><ul id='review-list'></ul></div>");
-    //$.each(reviewer, function(reviewer_index, reviewer_entry) {
+    var reviewer = services["review"]
+    $("#services").append("<div id='review'><ul id='review-list'></ul></div>");
+    $.each(reviewer, function(reviewer_index, reviewer_entry) {
       //$("#review-list").append("<li>" + reviewer_entry + "</li>");
-    //}
+    }
 
     //	initialize code page
     var codes = data["codes"];
